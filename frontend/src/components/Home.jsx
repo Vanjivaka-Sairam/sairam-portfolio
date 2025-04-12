04098c4ac3fbeec8be122cf9af629aa15d8861cf
Vanjivaka Sairam <sairamvanjivaka@gmail.com>
2025-04-12 16:12:52 +0530

    corrected the download cv probblem

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <h4 className="text-2xl text-blue-400 mb-4">And I'm a Frontend Developer</h4>
         <p className="text-lg max-w-md mx-auto mb-6">I am a passionate developer with a love for creating beautiful and functional web applications. My journey in tech 
           started with a curiosity for how things work and has grown into a fulfilling career.</p>
-        <a href="/assets/your-cv.pdf" download className="mt-6 px-10 py-4 bg-blue-500 text-white text-lg font-semibold rounded-full hover:bg-blue-600 hover:shadow-lg transition-transform transform hover:scale-105">Download CV</a>
+        <a href="../assets/Sairam_Vanjivaka_CV.pdf" download className="mt-6 px-10 py-4 bg-blue-500 text-white text-lg font-semibold rounded-full hover:bg-blue-600 hover:shadow-lg transition-transform transform hover:scale-105">Download CV</a>
       </div>
     </div>
   );
