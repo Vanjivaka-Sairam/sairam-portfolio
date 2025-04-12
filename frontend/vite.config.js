2c15e7e6786da62df8d6a15680fceec39370d6fc
Vanjivaka Sairam <sairamvanjivaka@gmail.com>
2025-04-12 20:29:34 +0530

    normal

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   base: '/sairam-portfolio/',
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sairam-portfolio/' : '/',
+  plugins: [react()]
+}))
